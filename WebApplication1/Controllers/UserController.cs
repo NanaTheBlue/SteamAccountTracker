06ff53cb0d65332a27a527d2a81ea78f80ca693d
SkylerBanana <Skyler_Banana@protonmail.com>
2026-06-04 10:14:18 -0400

    UserController: Login

--- WebApplication1/Controllers/UserController.cs
+++ WebApplication1/Controllers/UserController.cs
@@ -43,10 +43,38 @@ namespace WebApplication1.Controllers
         }
 
 
+        [HttpPost]
+        public async Task<IActionResult> Login([FromBody] LoginRequest loginRequest)
+        {
+            if (loginRequest == null)
+            {
+                return BadRequest("Request payload cannot be null.");
+            }
+
+            try
+            {
+                var result = await _userService.LoginUser(loginRequest);
+
+                if (result.Success == false) {
+
+                    return BadRequest(result.ErrorMessage);
+                }
+                
+
+                return Ok("User Login");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"HandlerException: {e.Message}");
+                return StatusCode(500, "An error occurred while loging in the user.");
+            }
+        }
 
 
 
-      
+
+
+
 
 
 
